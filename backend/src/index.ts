e66a2dbb6bfa7eff5a73d8609c2dcd52ffbe04fc
lancerdonnie <jideadedejifirst@gmail.com>
2022-02-11 17:19:44 +0100

    chore: deploying lambda

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,25 +1,32 @@
 // import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import type { APIGatewayProxyEvent, Handler } from 'aws-lambda';
+import type {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  Handler,
+} from 'aws-lambda';
 
 // const client = new DynamoDBClient({});
 
 const headers = {
   'Content-Type': 'application/json',
 };
-
-export const prime: Handler = async (event: APIGatewayProxyEvent, context) => {
-  // try {
-  //   // body = await dynamo
-  //   //   .scan({ TableName: event.queryStringParameters.TableName })
-  //   //   .promise();
-  //   // break;
-  //   // body = await dynamo.put(JSON.parse(event.body)).promise();
-  //   // break;
-  // } catch (err) {}
-
-  return {
-    statusCode: 200,
-    body: { hello: 'hello world' },
-    headers,
-  };
+export const prime: Handler = async (event: any) => {
+  try {
+    // body = await dynamo
+    //   .scan({ TableName: event.queryStringParameters.TableName })
+    //   .promise();
+    // break;
+    // body = await dynamo.put(JSON.parse(event.body)).promise();
+    // break;
+    return {
+      statusCode: 200,
+      body: { hello: 'hello world' },
+      headers,
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: err,
+    };
+  }
 };
